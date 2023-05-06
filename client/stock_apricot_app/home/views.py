852692ccaa754a56825a6be42b97913e2910aaf1
samarth1029 <samarthmishra291@gmail.com>
2023-05-06 21:53:14 +0530

    create function for respective views

--- client/stock_apricot_app/home/views.py
+++ client/stock_apricot_app/home/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from datetime import datetime
+from home.models import Contact
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return HttpResponse("this is services page")
+    # return render(request, 'services.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        _contact = Contact(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+            date=datetime.now())
+        _contact.save()
+        messages.success(request, 'Your message has been sent')
+    return render(request, 'contact.html')
