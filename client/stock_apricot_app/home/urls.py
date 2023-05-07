f8563f6783fd465eb718e65734910f19b6e6af08
samarth1029 <samarthmishra291@gmail.com>
2023-05-07 12:31:01 +0530

    add respective urls for gainers, losers and weekly report

--- client/stock_apricot_app/home/urls.py
+++ client/stock_apricot_app/home/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
     path("services", views.services, name="services"),
-    path("contact", views.contact, name="contact")
+    path("contact", views.contact, name="contact"),
+    path("weekly_report", views.weekly_report, name="weekly_report"),
+    path("gainers", views.gainers, name="gainers"),
+    path("losers", views.losers, name="losers"),
 ]
